6aa61de82e1f7438041027e59ec90de948c7a8a9
Vattet <156902457+Vattet@users.noreply.github.com>
2024-09-16 17:01:05 +0200

    first change

--- view/connexion.php
+++ view/connexion.php
@@ -7,13 +7,53 @@
   <link rel="stylesheet" href="../public/css/style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <title>Back Log</title>
+  <title>Sing in</title>
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
     rel="stylesheet">
   <link rel="icon" type="image/png" href="../public/img/abyssicon.png">
   <style>
     body {
       background-color: black;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      font-family: "Josefin Sans", sans-serif;
+    }
+    .FormSign{
+      background-color: white;
+      width: 29rem;
+      position: relative;
+      border-radius: 15px 15px 15px 15px;
+      margin: -4px;
+    }
+
+    .Divtilte{
+      background-color: white;
+      width: 7rem;
+      padding: 0 1vh 0 0;
+      border-radius: 15px 15px 0 0;
+      display: flex;
+      justify-content: center;
+      margin: -30px;
+      z-index: 200;
+    }
+    
+    .DivAll {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .form{
+      padding: 10%;
+      
+    }
+    .inputText{
+      border-radius: 10px;
+      margin-top:5px;
+      width: 20rem;
+      padding: 2%;
     }
   </style>
 </head>
@@ -22,17 +62,17 @@
   <nav class="">
     <img src="../public/img/icon.png" alt="Abyss" class="logo">
   </nav>
-  <div class="containerForm">
-    <div>
-      <h1 class="fontSignIn">Sign in</h1>
+  <div class="DivAll">
+    <div class="Divtilte" >
+      <h3 >Login</h3>
     </div>
-    <div>
-      <form action="home-user.php" method="post">
-        <div >
+    <div class="FormSign">
+      <form action="home-user.php" class="form" method="post">
+        <div style="padding: 5%;">
           <label for="email">Email</label>
           <input class="inputText" type="email" name="email" id="email" placeholder="Email" required>
         </div>
-        <div>
+        <div style="padding: 5%;">
           <label for="password">Password</label>
           <input class="inputText" type="password" id="password" name="password" placeholder="Password" required>
         </div>
--- view/register.php
+++ view/register.php
@@ -7,33 +7,76 @@
     <link rel="stylesheet" href="../public/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <title>Connexion</title>
+    <title>Register</title>
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
         rel="stylesheet">
     <link rel="icon" type="../image/png" href="../public/img/abyssicon.png">
+    <style>
+        .FormRegister {
+            background-color: white;
+            border-radius: 0 15px 15px 15px;
+            width: 50%;
+            padding: 2%;
+        }
+
+        .FormRegister{
+            
+        }
+        label {
+            display: flex;
+            padding: 4%;
+            margin: 10%;
+        }
+
+        body {
+            background-color: black;
+            width: 100%;
+            height: 100%;
+        }
+
+        .containerSign {
+            width: 40vh;
+        }
+
+        .h3 {
+            background-color: white;
+            width: 12vh;
+            padding: 5px;
+            border-radius: 15px 15px 0 0;
+
+        }
+
+        .divcenter {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+        }
+    </style>
 </head>
 
 <body>
     <nav>
-        <img src="../public/img/icon.png" alt="">
+        <img src="../public/img/icon.png" class="logo" alt="">
     </nav>
-    <div class="container-sign">
-        <div>
+    <div class="divcenter">
+        <div class="h3">
             <h3>Register</h3>
         </div>
-        <form action="login.php" method="post">
+        <form action="login.php" class="FormRegister" method="post">
 
             <div>
                 <label for="name">Name</label>
-                <input type="text" name="name" id="name">
+                <input type="text" name="name" placeholder="Name" id="name">
 
                 <label for="firstname">First Name</label>
-                <input type="text" name="firstname" id="firstname">
+                <input type="text" name="firstname" placeholder="First Name" id="firstname">
             </div>
 
             <!--a faire autrement -->
             <div>
                 <label for="gender">Gender</label>
+
                 <select name="gender" required>
                     <option value="man">man</option>
                     <option value="woman">woman</option>
@@ -41,26 +84,27 @@
                 </select>
 
                 <!--a faire autrement -->
-                <label for="Age">Age</label>
+                <label for="age">Age</label>
+                <input type="text" name="age" placeholder="Age" id="age" required>
 
                 <label for="phone">Phone Number</label>
-                <input type="text" name="phone" id="phone" required>
+                <input type="text" name="phone" placeholder="Phone Number" id="phone" required>
             </div>
 
             <div>
                 <label for="email">Email</label>
-                <input type="email" name="email" id="email" required>
+                <input type="email" name="email" placeholder="Email" id="email" required>
 
                 <label for="username">Username</label>
-                <input type="text" name="username" id="username" required>
+                <input type="text" name="username" placeholder="Username" id="username" required>
             </div>
 
             <div>
                 <label for="password">Password</label>
-                <input type="password" name="password" id="password">
+                <input type="password" name="password" placeholder="Password" id="password">
 
                 <label for="passwordbis">Passeword confirm</label>
-                <input type="password" name="passwordbis" id="passwordbis" required>
+                <input type="password" name="passwordbis" placeholder="Passeword confirm" id="passwordbis" required>
             </div>
 
             <div>
